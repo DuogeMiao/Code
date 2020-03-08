3a7132c4cccf49783d297acc60c600a8d2db6033
lz <lizhiqaz@gmail.com>
2020-03-08 08:24:39 +0800

    测试

--- Java/HelloWorld/src/com/tingshan/Demo/HelloWorld.java
+++ Java/HelloWorld/src/com/tingshan/Demo/HelloWorld.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("请输入你的名字：");
+        System.out.println("请输入你的名字：倒垛");
         Scanner sc=new Scanner(System.in);
         String s = sc.nextLine();
         System.out.println("Hello World\n"+s);
