765a5d9cbfaf183f405e77b640fae9c1a30f9bbc
lz <lizhiqaz@gmail.com>
2020-03-08 08:16:48 +0800

    测试

--- Java/HelloWorld/src/com/tingshan/Demo/HelloWorld.java
+++ Java/HelloWorld/src/com/tingshan/Demo/HelloWorld.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("请输入你的名字：");
         Scanner sc=new Scanner(System.in);
         String s = sc.nextLine();
+        System.out.println("Hello World\n"+s);
+        System.out.println("Hello World\n"+s);
         System.out.println("Hello World\n"+s);
         System.out.println("Hello World\n"+s);
         System.out.println("Hello World\n"+s);
